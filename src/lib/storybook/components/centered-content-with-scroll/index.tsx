7c5793280c2d89aa0445bdb544fab6ec1bb3d268
Roman Akhmadullin <akhmadullin_roman@mail.ru>
2024-09-22 19:16:49 +1000

    Fix popover stories sizes outside docs

--- src/lib/storybook/components/centered-content-with-scroll/index.tsx
+++ src/lib/storybook/components/centered-content-with-scroll/index.tsx
@@ -1,8 +1,12 @@
 import React, { FC, PropsWithChildren, useRef, useLayoutEffect } from 'react';
+import cx from 'classnames';
 import classes from './styles.module.css';
 
 const CenteredContentWithScroll: FC<PropsWithChildren> = ({ children }) => {
     const containerRef = useRef<HTMLDivElement | null>(null);
+
+    const withoutBorder = location ? new URLSearchParams(location.search).get('withoutBorder') : false;
+    const fixedHeight = location ? new URLSearchParams(location.search).get('fixedHeight') : null;
 
     useLayoutEffect(() => {
         containerRef.current?.scrollTo({
@@ -13,7 +17,15 @@ const CenteredContentWithScroll: FC<PropsWithChildren> = ({ children }) => {
     }, []);
 
     return (
-        <div ref={containerRef} className={classes.container}>
+        <div
+            ref={containerRef}
+            className={cx(classes.container, {
+                [classes.containerWithoutBorder]: withoutBorder,
+                [classes.containerWithFixedHeight]: Boolean(fixedHeight),
+                [classes.containerFullScreen]: !fixedHeight,
+            })}
+            style={{ height: fixedHeight ?? undefined }}
+        >
             <div className={classes.wrapper}>
                 <div className={classes.contentContainer}>{children}</div>
             </div>
